97c5b67e2ed8cbc7c5c63ae3e3e8850fa9d0585d
dancebutter <ken.zhangtq@gmail.com>
2016-08-25 14:40:30 +0800

    add angular ui router

---
+++ js/gamePage.controller.js
@@ -0,0 +1,72 @@
+myApp.controller('gamePageCtrl', ['$scope', function($scope) {
+  $scope.summonerList = {
+    'uncletai' : {
+      nickName : '我',
+      summonerName : 'uncletai',
+      summonerId : 35793439,
+      actived : true
+    },
+    'geminiroy' : {
+      nickName : '罗伊',
+      summonerName : 'geminiroy',
+      summonerId : 35448330,
+      actived : false
+    },
+    'personalbusiness' : {
+      nickName : '二轮',
+      summonerName : 'personalbusiness',
+      summonerId : 35468371,
+      actived : false
+    },
+    '00Mirana00' : {
+      nickName : '拉里',
+      summonerName : '00Mirana00',
+      summonerId : 25857364,
+      actived : false
+    },
+    'woaichibaozi' : {
+      nickName : '李老师',
+      summonerName : 'woaichibaozi',
+      summonerId : 32524746,
+      actived : false
+    },
+    'AsapAoi' : {
+        nickName : '变态儿童',
+        summonerName : 'AsapAoi',
+        summonerId : 69539468,
+        actived : false
+    }
+  };
+  $scope.activedSummoner = $scope.summonerList.uncletai;
+  $scope.testTemp = null;
+  $scope.gameListShow = true;
+  $scope.gameDetailShow = true;
+  $scope.activedGameId = null;
+
+  // events
+  $scope.$watch('activedSummoner', function( newValue, oldValue ) {
+    if( oldValue !== newValue ) {
+      oldValue.actived = false;
+      newValue.actived = true;
+      // updateGameList();
+    }
+  });
+  $scope.$watch('activedGameId', function( newValue, oldValue ) {
+    if( newValue != null && newValue != oldValue ) {
+
+    }
+  });
+  // $scope.$on('gameDetail:showGameDetail', function( event, gameId, summoner ) {
+  //   $scope.currenctGameId = gameId;
+  // });
+  // events end
+
+  // functions
+  $scope.clickName = function( summoner ) {
+    $scope.activedSummoner = summoner;
+  };
+  $scope.submitGameData = function( gameDataJsonStr ) {
+    $scope.gameDataJson = JSON.parse( gameDataJsonStr );
+  };
+  // functions end
+}]);
--- js/myApp.js
+++ js/myApp.js
@@ -1,1 +1,15 @@
-var myApp = angular.module('myApp', ['ngResource']);
+var myApp = angular.module('myApp', ['ngResource', 'ui.router']);
+
+myApp.config(['$stateProvider', '$urlRouterProvider', function($stateProvider, $urlRouterProvider) {
+  $urlRouterProvider.otherwise('/gamePage');
+
+  $stateProvider
+    .state('gamePage', {
+      url : "/gamePage",
+      templateUrl : "js/gamePage.html"
+    })
+    .state('otherPage', {
+      url : "/otherPage",
+      templateUrl : "js/otherPage.html"
+    });
+}]);
