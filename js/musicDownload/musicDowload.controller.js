db5efb38966948560fe971628af7951b9108515e
dancebutter <ken.zhangtq@gmail.com>
2016-09-01 14:48:28 +0800

    first draft of music. use iframe

--- js/musicDownload/musicDowload.controller.js
+++ js/musicDownload/musicDowload.controller.js
@@ -2,14 +2,21 @@ MusicDownloadCtrl.$inject = ["$scope", "musicDownloadService"];
 function MusicDownloadCtrl( $scope, MusicD ) {
   $scope.musicName = "";
   $scope.searchMusicByName = function() {
-    // console.log("Music Name is : " + $scope.musicName);
-    MusicD.getMusicListByName($scope.musicName)
-    .then(function(data) {
-      console.log(data);
-    },
-    function(error) {
-      console.log(error);
-    });
+    console.log("Music Name is : " + $scope.musicName);
+    // MusicD.getMusicListByName($scope.musicName)
+    // .then(function(data) {
+    //   console.log(data);
+    // },
+    // function(error) {
+    //   console.log(error);
+    // });
+    var urlPrefix = "http://musicmini.baidu.com/app/search/searchList.php?qword={";
+    var urlSurfix = "}&ie=utf-8&page={1}";
+    var musicSearchUrl = urlPrefix + $scope.musicName + urlSurfix;
+
+    $scope.musicFrame = angular.element(document.getElementById("musicSearchList"));
+    $scope.musicFrame.attr('src', musicSearchUrl);
+
   };
 }
 myApp.controller('musicDownloadCtrl', MusicDownloadCtrl);
--- js/musicDownload/musicDownload.service.js
+++ js/musicDownload/musicDownload.service.js
@@ -5,13 +5,14 @@ function MusicDownloadService( $q, $http ) {
   };
   return service;
   function getMusicListByName( musicName ) {
+    // TODO: This function is NOT working!!!
     if( musicName ) {
       // "http://mp3.baidu.com/dev/api/?tn=getinfo&ct=0&word=%E6%B5%81%E6%B5%AA%E8%AE%B0&ie=utf-8&format=json"
-      // var urlPrefix = "http://musicmini.baidu.com/app/search/searchList.php?qword={";
-      // var urlSurfix = "}&ie=utf-8&page={1}";
+      var urlPrefix = "http://musicmini.baidu.com/app/search/searchList.php?qword={";
+      var urlSurfix = "}&ie=utf-8&page={1}";
 
-      var urlPrefix = "http://mp3.baidu.com/dev/api/?tn=getinfo&ct=0&word=";
-      var urlSurfix = "&ie=utf-8&format=json";
+      // var urlPrefix = "http://mp3.baidu.com/dev/api/?tn=getinfo&ct=0&word=";
+      // var urlSurfix = "&ie=utf-8&format=json";
       var url = urlPrefix + musicName + urlSurfix;
 
       // var url = "http://ting.baidu.com/data/music/links?songIds={247911654}";
@@ -19,19 +20,22 @@ function MusicDownloadService( $q, $http ) {
       var deferred = $q.defer();
       var httpConfig = {
         url : url,
-        method : 'GET',
-        dataType : 'JSONP',
-        success : function(data) {
-          deferred.resolve(data);
+        // method : 'GET',
+        headers : {
+          'Access-Control-Allow-Origin' : '*'
         }
+        // dataType : 'JSONP',
+        // success : function(data) {
+        //   deferred.resolve(data);
+        // }
       };
-      $http(httpConfig);
-      // .success(function(data) {
-      //   deferred.resolve(data);
-      // })
-      // .error(function(error) {
-      //   deferred.reject(error);
-      // });
+      $http(httpConfig)
+      .success(function(data) {
+        deferred.resolve(data);
+      })
+      .error(function(error) {
+        deferred.reject(error);
+      });
       return deferred.promise;
     }
   };
