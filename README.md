00bdb6e89fad33cb3688f277342d8cc3dd281b45
dancebutter <ken.zhangtq@gmail.com>
2017-06-29 16:54:19 +1000

    Add Excel reader

--- README.md
+++ README.md
@@ -1,6 +1,11 @@
 # dancebutter.github.io
 A playground for me.
 ## Change Log
+##### 06/29/2017
+Code clean up:
+1. Remove peppa pig page
+2. Remove time table page
+3. Add report generator page
 ##### 09/29/2016
 Add Peppa Pig page abc. Each character has its own picture. Click on each picture,
 will have a piece of introduce voice.
--- js/reportGene/reportGene.controller.js
+++ js/reportGene/reportGene.controller.js
@@ -28,5 +28,20 @@ function ReportGeneCtrl($scope) {
     $scope.clickPage = function( page ) {
       $scope.activedPage = page;
     };
+
+    function fileSelected() {
+      var file = document.getElementById('fileToUpload').files[0];
+      if (file) {
+        // var fileSize = 0;
+        // if (file.size > 1024 * 1024)
+        //   fileSize = (Math.round(file.size * 100 / (1024 * 1024)) / 100).toString() + 'MB';
+        // else
+        //   fileSize = (Math.round(file.size * 100 / 1024) / 100).toString() + 'KB';
+
+        // document.getElementById('fileName').innerHTML = 'Name: ' + file.name;
+        // document.getElementById('fileSize').innerHTML = 'Size: ' + fileSize;
+        // document.getElementById('fileType').innerHTML = 'Type: ' + file.type;
+      }
+    }
 }
 myApp.controller('reportGeneCtrl', ReportGeneCtrl);
